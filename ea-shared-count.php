62e31ce01f0dc8e0074fb54863d697a655d4466b
Jared Atchison <jared@jaredatchison.com>
2015-09-16 12:22:35 -0500

    ea_share_count_single_image adjustment

--- ea-shared-count.php
+++ ea-shared-count.php
@@ -376,12 +376,8 @@ final class EA_Share_Count {
 				$link['url']   = get_permalink( $id );
 				$link['title'] = get_the_title( $id );
 				$img           = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'full' );
-				if ( isset( $img[0] ) ) {
-					$link['img'] = $img[0];
-				} else {
-					$link['img'] = apply_filters( 'ea_share_count_default_image', '' );
-				}
-				$link['img'] = apply_filters( 'ea_share_count_single_image', $link['img'], $id );
+				$link['img']   = isset( $img[0] ) ? $img[0] : '';
+				$link['img']   = apply_filters( 'ea_share_count_single_image', $link['img'], $id );
 			}
 			$link['count'] = $this->count( $id, $type, false, $round );
 
