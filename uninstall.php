51d412cd92a33c1170aec24c1417e6f179346104
Jared Atchison <jared@jaredatchison.com>
2015-10-06 15:23:32 -0500

    Add uninstall functionality. Closes #21.

---
+++ uninstall.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Uninstall EA Share Count
+ *
+ * @package    EA_ShareCount
+ * @author     Bill Erickson & Jared Atchison
+ * @since      1.2.0
+ * @license    GPL-2.0+
+ * @copyright  Copyright (c) 2015
+ */
+ 
+// Exit if accessed directly
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
+
+global $wpdb;
+
+// Remove all plugin options
+$wpdb->query( "DELETE FROM `{$wpdb->options}` WHERE `option_name` LIKE ('ea\_share\_count\_%')" );
+
+// Remove all plugin post_meta keys
+$wpdb->query( "DELETE FROM `{$wpdb->postmeta}` WHERE `meta_key` LIKE ('ea\_share\_count\_%')" );
